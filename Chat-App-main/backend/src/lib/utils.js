8dea2e1de9cf3aaf1d4e35b4212302aaeb2aaf80
sanskar225 <sanskarsinha225@gmail.com>
2025-11-30 09:26:42 +0530

    Refactor generateToken function for improved cookie handling

--- Chat-App-main/backend/src/lib/utils.js
+++ Chat-App-main/backend/src/lib/utils.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
-
+/* eslint-env node */ 
 export const generateToken = (userId, res)=>{
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    const token = jwt.sign({id:userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
-    })
+    });
 
     res.cookie("jwt", token, {
         httpOnly: true,
         secure:true,
-        sameSite: "none",
+        sameSite: "None",
         path:"/",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
