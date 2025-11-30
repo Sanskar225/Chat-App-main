7bada61881581ced6a8a67e6aca87d33576b8a57
sanskar225 <sanskarsinha225@gmail.com>
2025-11-30 10:34:36 +0530

    Refactor generateToken function for improved cookie security

--- Chat-App-main/backend/src/lib/utils.js
+++ Chat-App-main/backend/src/lib/utils.js
@@ -6,10 +6,9 @@ export const generateToken = (userId, res)=>{
     });
 
     res.cookie("jwt", token, {
-        httpOnly: false,
+        httpOnly: true,
         secure:true,
         sameSite: "None",
-        path:"/",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     return token;
